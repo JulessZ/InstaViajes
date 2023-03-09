e99f2a49264b7fba48b829f06be99daec7395123
saragm94 <saraglezm94@gmail.com>
2023-03-09 19:51:57 +0100

    Añade estructura básica de las plantillas: login, registro y reset pass

--- InstaViajes_frontend/plantillas/passwordreset.js
+++ InstaViajes_frontend/plantillas/passwordreset.js
@@ -1,3 +1,9 @@
 let passwordreset = `
-<h1>pagina de resetear paswword/h1>
+<div class="container text-center" id="form-reset">
+  <div class="row align-items-center" style="min-height: 100vh">
+    <div class="col-12 border border-primary">
+     
+    </div>
+  </div>
+</div>
 `
