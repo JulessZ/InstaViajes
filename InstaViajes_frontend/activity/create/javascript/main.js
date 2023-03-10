8fcb9f5c75949d25c7e0ac3f4872766538ef94dc
Marco <tonilopzexposito@gmail.com>
2023-03-10 18:29:02 +0100

    Formulario de creacion creado.
    Se ha creado un formulario para crear una nueva actividad, con validaciones para comprobar si cada campo se encuentra vacio.
    Es necesario terminar el datalist que es usado a modo de buscador para filtrar el fetch que nos llegue con datos para el input 'Nombre'.

---
+++ InstaViajes_frontend/activity/create/javascript/main.js
@@ -0,0 +1,80 @@
+const inputName = document.getElementById('inputName');
+const inputDescription  = document.getElementById('inputDescription');
+const inputDateStart = document.getElementById('inputDateStart');
+const inputHourStart = document.getElementById('inputHourStart');
+const inputDuration = document.getElementById('inputDuration');
+const inputPrice = document.getElementById('inputPrice');
+
+function checkVoidInputAndSaveValues() {
+
+    let newActivity = {
+        name: '',
+        description: '',
+        date: '',
+        hour: '',
+        duration: '',
+        price: ''
+    };
+
+    if(inputName.value == '') {
+        window.alert('Nombre vacio');
+        return;
+    }
+    else {
+        newActivity.name = inputName.value;
+    }
+
+    if(inputDescription.value == '') {
+        window.alert('Descripcion vacia');
+        return;
+    }
+    else {
+        newActivity.description = inputDescription.value;
+    }
+
+    if(inputDateStart.value == '') {
+        window.alert('Fecha vacia');
+        return;
+    }
+    else {
+        newActivity.date = inputDateStart.value;
+    }
+
+    if(inputHourStart.value == '') {
+        window.alert('Hora vacia');
+        return;
+    }
+    else {
+        newActivity.hour = inputHourStart.value;
+    }
+
+    if(inputDuration.value == '') {
+        window.alert('Duracion vacia');
+        return;
+    }
+    else {
+        newActivity.duration = inputDuration.value;
+    }
+
+    if(inputPrice.value == '') {
+        window.alert('Precio vacio');
+        return;
+    }
+    else {
+        newActivity.price = inputPrice.value;
+    }
+
+    activitiesList.push(newActivity);
+
+}
+
+
+let activitiesList = [];
+
+const buttonAddActivity = document.getElementById('buttonAddActivity');
+
+buttonAddActivity.addEventListener('click',()=>{
+
+    checkVoidInputAndSaveValues();
+
+});
