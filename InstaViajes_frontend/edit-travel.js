a6f28feadf139a0d6a1a923945510013d12c55b0
Alejandro Alberola <alejandroalberola140495@gmail.com>
2023-03-10 18:29:51 +0100

    Implementado buscador de amigos y amigos en el viaje
    Falta por implementar la eliminación de amigos de un viaje y ajustes de estructura planteados por donovan

--- InstaViajes_frontend/edit-travel.js
+++ InstaViajes_frontend/edit-travel.js
@@ -9,23 +9,30 @@ const trip1 = {
     description: "El viaje consistirá en un mes de blablablalbal",
     budget: 3000,
     estado: 'abierto',
-    friends: [
-        { id: 1, name: 'diegoB', mail: 'diego@example.com', image: 'image.png' },
-        { id: 2, name: 'brian', mail: 'brian@example.com', image: 'image.png' },
-        { id: 3, name: 'diegoR', mail: 'diego@example.com', image: 'image.png' },
-        { id: 4, name: 'aram', mail: 'aram@example.com', image: 'image.png' },
-        { id: 5, name: 'carmen', mail: 'carmen@example.com', image: 'image.png' },
-        { id: 6, name: 'sara', mail: 'sara@example.com', image: 'image.png' },
-        { id: 7, name: 'rai', mail: 'rai@example.com', image: 'image.png' },
-        { id: 8, name: 'jorge', mail: 'jorge@example.com', image: 'image.png' },
-        { id: 9, name: 'marco', mail: 'marco@example.com', image: 'image.png' },
-        { id: 10, name: 'donovan', mail: 'donovan@example.com', image: 'image.png' }
-      ]
-  };
+    //Var to introduce friends into the trip
+    friendsOnTrip : [
+        {id: 1, image: 'imageDiegoB.png' },
+        {id: 2, image: 'imageBrian.png' }
+    ]
+    
+}; 
+const friends = [
+    { id: 3, name: 'diegoR', mail: 'diego@example.com', image: 'image.png' },
+    { id: 4, name: 'aram', mail: 'aram@example.com', image: 'image.png' },
+    { id: 5, name: 'carmen', mail: 'carmen@example.com', image: 'image.png' },
+    { id: 6, name: 'sara', mail: 'sara@example.com', image: 'image.png' },
+    { id: 7, name: 'rai', mail: 'rai@example.com', image: 'image.png' },
+    { id: 8, name: 'jorge', mail: 'jorge@example.com', image: 'image.png' },
+    { id: 9, name: 'marco', mail: 'marco@example.com', image: 'image.png' },
+    { id: 10, name: 'donovan', mail: 'donovan@example.com', image: 'image.png' }
+];
   
+
 //Const parent to insert the form
 const editContainer = document.querySelector('#form-editar-viajes');
+//Div mains of searcher realted to friends
 const divMainSearch = document.createElement('div');
+const friendsOnTrip = document.createElement('div');
 
 //Function to create edit-Form
 function editForm(object) {
@@ -107,10 +114,10 @@ function editForm(object) {
         const labelBudget = document.createElement('label');
         const budget = document.createElement('input');
         const budgetValue = document.createElement('span');
-        const friends = document.createElement('select');
-        const friendsDescription = document.createElement('option');
+        const labelFriendSearch = document.createElement('label');
         const friendsSearch = document.createElement('input');
         const submitButton = document.createElement('input');
+        const cancelButton = document.createElement('input');
 
 
         //Setting the attributes to the different elements
@@ -175,21 +182,12 @@ function editForm(object) {
             budgetValue.innerHTML = budget.value +'€';
         });
 
-        friends.setAttribute('name' , 'friends');
-        friendsDescription.setAttribute('selected', 'true');
-        friendsDescription.setAttribute('disabled', 'true');
-        //Inserting the selected disabled option on the html
-        friendsDescription.textContent = "Invitar Amigos";
-        //Obtaining the friends values from object array
-        friends.appendChild(friendsDescription);
-        (object.friends).forEach(element => {
-            const optionFriends = document.createElement('option');
-            optionFriends.value = element.id;
-            optionFriends.textContent = element.name;
-            friends.appendChild(optionFriends);
-        });
+        friendsOnTrip
 
+        labelFriendSearch.setAttribute('for', 'search');
+        labelFriendSearch.textContent = 'Invitar Amigos';
         friendsSearch.type = 'search';
+        friendsSearch.name = 'search';
 
 
         //Adding the event listener to search into the friends of an user
@@ -197,9 +195,9 @@ function editForm(object) {
             removeAllChilds(divMainSearch);
             if ((friendsSearch.value).length != 0) {
                 //If we obtain the value needed we print the divs 
-                (object.friends).filter(element =>  {
+                (friends).filter(element =>  {
                     if ((element.name).includes(friendsSearch.value)) {
-                        createNameSearch(element.image, element.name);
+                        createNameSearch(element.id,element.image, element.name);
                     }
                 }) 
             }
@@ -207,6 +205,9 @@ function editForm(object) {
 
         submitButton.type = 'submit';
         submitButton.value = 'Editar viaje';
+
+        cancelButton.type = 'reset';
+        cancelButton.value = 'Cancelar';
 
         //Inserting the elements into de html
         editContainer.appendChild(form);
@@ -232,12 +233,17 @@ function editForm(object) {
         form.appendChild(budget);
         form.appendChild(budgetValue);
 
-        form.appendChild(friends);
+        form.appendChild(friendsOnTrip);
 
+        form.appendChild(labelFriendSearch);
         form.appendChild(friendsSearch);
         form.appendChild(divMainSearch);
 
         form.appendChild(submitButton);
+
+        form.appendChild(cancelButton);
+
+        updateTripFriends(object);
     }
     
 }
@@ -247,7 +253,7 @@ editForm(trip1);
 
 
 //Function to create the elements of the search
-function createNameSearch(img,name) {
+function createNameSearch(id,img,name) {
     //Giving the necesary elements
     const divNameSearch = document.createElement('div');
     const divImg = document.createElement('div');
@@ -259,6 +265,20 @@ function createNameSearch(img,name) {
     imgSearch.alt = 'img';
     divName.textContent = name;
 
+
+    divNameSearch.addEventListener('click', function() {
+        const element = {
+            id: id,
+            image: img
+        }
+        if (searchOnTrip(element)) {
+            window.alert('No se puede insertar este amigo ya que se encuentra en el viaje');
+        } else {
+            (trip1.friendsOnTrip).push(element);
+            updateTripFriends(trip1);
+            console.log(trip1.friendsOnTrip); 
+        }
+    });
     //Insert the elements into html
     divMainSearch.appendChild(divNameSearch);
     divNameSearch.appendChild(divImg);
@@ -272,4 +292,34 @@ function removeAllChilds(div) {
     while (div.firstChild) {
         div.removeChild(div.firstChild);
     }
+}
+
+//Function to submit the edit-form
+function submitEditForm (e){
+    e.preventDefault();
+}
+
+//Function to load the friends already on trip
+function updateTripFriends (object) {
+    //we remove the childs of the element
+    removeAllChilds(friendsOnTrip);
+    //Then we charge the elements in trip
+    (object.friendsOnTrip).forEach(element => {
+        const friendsOnTripImg = document.createElement('img');
+        friendsOnTripImg.id = element.id;
+        friendsOnTripImg.src = element.image;
+        friendsOnTrip.appendChild(friendsOnTripImg);
+
+        //friendsOnTripImg.setAttribute('onclick', 'deleteFriendsTrip(this)');
+    });
+}
+
+//Function to delete friends from the trip
+function deleteFriendsTrip (element) {
+    console.log(element.id);
+}
+
+//Function to search if the friend is on the trip
+function searchOnTrip(element) {
+    return trip1.friendsOnTrip.some(friend => friend.id === element.id);
 }
