3afe27448f019e91db50bf27638f1fc2f9771d7d
Donovanmar <donovanmar30@gmail.com>
2023-03-12 23:52:54 +0000

    Corrige vistas de creación y edición de viajes y actividades

--- InstaViajes_frontend/plantillas/editarviajes.js
+++ InstaViajes_frontend/plantillas/editarviajes.js
@@ -1,11 +1,11 @@
 export let editarviaje= `
     <div class="container">
         <div class="row">
-            <div class="col-12 border">
-                <h1>Edición del viaje</h1>
+            <div class="col-12">
+                <h1 class="style-form-title">Edición del viaje</h1>
             </div>
-            <div class="col-12 border" id="form-editar-viajes">
-                
+            <div class="col-12" id="formeditarviajes">
+            
             </div>
         </div>
     </div>
