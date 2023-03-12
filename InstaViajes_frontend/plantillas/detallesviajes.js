c9deb9d109780cfe1c8bca0e30f34559b4a76e65
Rai <jraicr@gmail.com>
2023-03-12 17:28:32 +0000

    Añade template y vista a la ruta de detalles viaje

--- InstaViajes_frontend/plantillas/detallesviajes.js
+++ InstaViajes_frontend/plantillas/detallesviajes.js
@@ -1,10 +1,12 @@
 //importacion del js con la funcionalidad para la vista detalles viaje
 
-
+  /* html */
 export let detallesviaje = `
+<div id="detallesviaje"></div>
+
 <div class="container-fluid text-center mt-5 border border-info" >
 <div class="row align-items-center  mx-lg-5  border border-danger">
-  <div class="col-12  mx-lg-5 border border-primary" id="detallesviaje">
+  <div class="col-12  mx-lg-5 border border-primary">
     <div id="carouselExampleInterval" class="carousel slide  m-lg-5  border border-danger" data-bs-ride="carousel">
       <div class="carousel-inner" id="carruselitinerario">
 
@@ -20,8 +22,10 @@ export let detallesviaje = `
         <span class="visually-hidden">Next</span>
       </button>
     </div>
-    <div id="detallesactividades"></div>
+   
   </div>
+  <div id="itinerary-list">test</div>
+  <div id="detallesactividades"></div>
 </div> 
 </div>
 `
