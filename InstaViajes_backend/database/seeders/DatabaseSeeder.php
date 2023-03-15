161aedea7d0eab3b00f16a50ba145b2fc4251c0a
Donovanmar <donovanmar30@gmail.com>
2023-03-15 12:34:00 +0000

    Añade factories de actividades y lugares, y los incorpora en el seeder

--- InstaViajes_backend/database/seeders/DatabaseSeeder.php
+++ InstaViajes_backend/database/seeders/DatabaseSeeder.php
@@ -4,7 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
+use App\Models\Activity;
 use App\Models\Friendship;
+use App\Models\Place;
 use App\Models\Post;
 use App\Models\Travel;
 use App\Models\TravelUsers;
@@ -26,6 +28,8 @@ class DatabaseSeeder extends Seeder
         $this->call(UserSeeder::class);
         TravelUsers::factory(100)->create();
         $this->call(TravelUsersSeeder::class);
+        Place::factory(100)->create();
+        Activity::factory(50)->create();
         // \App\Models\User::factory(10)->create();
     }
 }
