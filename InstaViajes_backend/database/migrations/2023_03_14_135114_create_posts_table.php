361f088634a53d1516c164ff229f332597d061f7
DiegoBritoSanchez <diegobritosanchez13@gmail.com>
2023-03-15 15:38:44 +0100

    El campo description de posts puede ser nulo

--- InstaViajes_backend/database/migrations/2023_03_14_135114_create_posts_table.php
+++ InstaViajes_backend/database/migrations/2023_03_14_135114_create_posts_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->foreign("user_id")->references("id")->on("users")->onDelete("cascade");
             $table->unsignedBigInteger("travel_id");
             $table->foreign("travel_id")->references("id")->on("travels")->onDelete("cascade");
-            $table->text('description', 1000);
+            $table->text('description', 1000)->nullable();
             $table->timestamps();
         });
     }
