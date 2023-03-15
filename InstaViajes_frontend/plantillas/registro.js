4bc7b845d91fe545ed3377c23676889be643e898
karmen1994 <karmen1271994@gmail.com>
2023-03-15 14:56:53 +0100

    añadido el estilo y la plantilla de registro(realizado por sara)

--- InstaViajes_frontend/plantillas/registro.js
+++ InstaViajes_frontend/plantillas/registro.js
@@ -1,7 +1,7 @@
 export let registro = `
-<div class="container text-center" id="form-registro">
-  <div class="row align-items-center" style="min-height: 100vh">
-    <div class="col-12 border border-primary">
+<div class="container text-center">
+  <div class="row align-items-center" style="min-height: 70vh">
+    <div class="col-12" id="form-registro">
       
     </div>
   </div>
