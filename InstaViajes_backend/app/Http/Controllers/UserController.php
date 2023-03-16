df111a38b8d168ddaab88c185179157e2dd18d14
brianbb0 <alvarezbbrian@gmail.com>
2023-03-16 15:29:04 +0100

    Cambios al Modelo y a la función
    - Ahora el modelo friendship tiene solo una unión mediante ID
    - Ahora los endpoints no rompen ante nulos

--- InstaViajes_backend/app/Http/Controllers/UserController.php
+++ InstaViajes_backend/app/Http/Controllers/UserController.php
@@ -86,6 +86,9 @@ class UserController extends Controller
 
     public function indexTravels(User $user)
     {
+        if (isset($user->travels)) {
+            return [];
+        }
         $travels = $user->travels->toArray();
         $newTravels = array_map(function ($travel) {
             // Calcula el número de días
@@ -141,8 +144,12 @@ class UserController extends Controller
     public function indexFriendTravels(User $user)
     {
         $friends = $user->friendships;
+        return $friends;
         foreach ($friends as $friend) {
-            return $friend;
+            $user = $friend->user;
+            if (isset($user->travels)) {
+                return [];
+            }
             $travels = $user->travels->toArray();
             $newTravels = array_map(function ($travel) {
                 // Calcula el número de días
@@ -163,7 +170,6 @@ class UserController extends Controller
                 if ($fotoUser) { // Evita errores
                     $fotoUser = Image::find($fotoUser)->value("name");
                 }
-
                 return [
                     'id' => $travel['id'],
                     'user_id' => $travel['user_id'],
